76b7e5528fe5cee1deed146ee50212ce49b51197
Palanivel <palanipsb@gmail.com>
2025-04-14 21:55:15 -0300

    update commit

--- WebApp/learningapp/obj/Release/net8.0/learningapp.AssemblyInfo.cs
+++ WebApp/learningapp/obj/Release/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe05db4c056f1fe7242a41092799b520d274927a")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
